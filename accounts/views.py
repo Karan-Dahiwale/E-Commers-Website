710a8d679b76a90fe092217776a85d2b8989d1ae
Karan dahiwale <karandahiwale80@gmail.com>
2025-09-21 13:18:01 +0530

    completed checkout functionality

--- accounts/views.py
+++ accounts/views.py
@@ -17,7 +17,7 @@ from django.core.mail import EmailMessage
 from carts.views import _cart_id
 from carts.models import Cart,CartItem
 import requests
-from urllib.parse import urlparse, parse_qs
+
 
 
 # Create your views here.
